6e9c90c0b860ea127d30eeca33b4694b1833c6db
yakov <yakovburtsev@gmail.com>
2019-06-19 09:52:36 +0300

    drawer

--- app/src/main/java/com/yaburtsev/catchat/MainActivity.java
+++ app/src/main/java/com/yaburtsev/catchat/MainActivity.java
@@ -1,7 +1,11 @@
 package com.yaburtsev.catchat;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Fragment fragment = new InboxFragment();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.add(R.id.content_frame, fragment);
+        ft.commit();
     }
 }
